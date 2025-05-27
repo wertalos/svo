d942d37822023e13053dfea4769d11bca12d6aba
CodeSandbox <support@codesandbox.io>
2025-05-27 09:51:48 +0000

    Initial commit for new repository

---
+++ svo-backend/src/main/java/ru/bisoft/svo/SvoApplication.java
@@ -0,0 +1,13 @@
+package ru.bisoft.svo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvoApplication.class, args);
+    }
+
+}
